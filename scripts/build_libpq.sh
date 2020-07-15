218b5f4e8ecfd44f102679335c83f64e172e68d0
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-07-15 12:36:08 +0530

    Update build_libpq.sh

--- scripts/build_libpq.sh
+++ scripts/build_libpq.sh
@@ -20,8 +20,8 @@ source ~/perl5/perlbrew/etc/bashrc
 set -eu
 perlbrew help
 yum install bzip2
-perlbrew install --notest perl
-perlbrew switch perl
+perlbrew install --notest perl-5.16.0
+perlbrew switch perl-5.16.0
 
 # Build openssl if needed
 OPENSSL_TAG="OpenSSL_${OPENSSL_VERSION//./_}"

1bb1dea678dbde06d9440c94afd6d6c771d750d0
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-07-16 19:52:37 +0530

    Update build_manylinux2014_aarch64.sh

--- scripts/build_manylinux2014_aarch64.sh
+++ scripts/build_manylinux2014_aarch64.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 # set -x
-
+echo $TRAVIS_BUILD_DIR
 docker run --rm -v $TRAVIS_BUILD_DIR:/build \
     -e PSYCOPG2_TESTDB_USER=postgres -e PSYCOPG2_TEST_FAST=1 \
     -e WANT_LIBPQ -e PACKAGE_NAME \

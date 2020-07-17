24e139f5c7cda43a3a3d5df6e0c6d30a9f4f00e0
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-07-17 14:39:59 +0530

    Update build_libpq.sh

--- scripts/build_libpq.sh
+++ scripts/build_libpq.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 set -x
 
 OPENSSL_VERSION="1.1.1d"
-LDAP_VERSION="2.4.48"
+LDAP_VERSION="2.4.50"
 SASL_VERSION="2.1.27"
 # If you change this, fix WANT_LIBPQ too in .travis.yml
 POSTGRES_VERSION="11.5"
